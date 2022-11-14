a930471b5c643f8776b396ed81da175beacd5121
Juan Portillo <juanportillog@gmail.com>
2022-11-15 00:35:17 +0100

    Initial commit

---
+++ src/api/accion/controllers/accion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * accion controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::accion.accion');
---
+++ src/api/accion/services/accion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * accion service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::accion.accion');
